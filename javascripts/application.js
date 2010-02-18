5cc04dcb4616abcab56291c664cc3a66d4e39790
Yves Van Broekhoven <yvesvanbroekhoven@gmail.com>
2010-02-18 10:48:07 +0100

    Updated meta tags, updated IE stylesheets

--- javascripts/application.js
+++ javascripts/application.js
@@ -7,13 +7,6 @@ Fueled by jQuery
 
 */
 
-// Note: No conflict function
-
-// This function must be called after including the jQuery javascript file, but before including any other conflicting library, 
-// and also before actually that other conflicting library gets used, in case jQuery is included last. noConflict can be called at 
-// the end of the jQuery.js file to globally disable the $() jQuery alias. jQuery.noConflict returns a reference to jQuery, so it 
-// can be used to override the $() alias of the jQuery object.
-// Use jQuery via jQuery(...)
 //jQuery.noConflict();
 
 jQuery(document).ready(function(){
